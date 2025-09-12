a0d74a51f45a45eb0ec069fc492daed3d5a3f3d0
maysadalila <maydepazcardenas@crackthecode.la>
2025-09-12 09:24:23 -0500

    quitamos la sección de la encuesta para reemplazarlo en un boton, (más detalles de los cambio en README)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 clicker.classList.toggle("p-red");
+
+var Question=[
+    "¿Cuál es tu nombre o apodo?",
+    "¿Qué edad tienes?",
+    "¿En qué país o ciudad vives?",
+    "¿Qué tipo de contenido te gusta compartir o ver? (moda, consejos, estilo de vida, arte, etc.)",
+    "¿Qué esperas encontrar al unirte?",
+]
