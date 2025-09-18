4bd1f3268c937eff799a39e14f746aaf94f06700
JazSantana <Marina@crackthecode.la>
2025-09-18 06:41:05 -0500

    Agregar ultimos detalles

--- assets/js/autocuidado.js
+++ assets/js/autocuidado.js
@@ -1,45 +1,48 @@
 let tipsAutocuidado = [
   {
     name: "Cuida tu mente",
-    description: "Escribe lo que sientes, haz pausas de redes y date permiso de descansar.",
-imgUrl: "../img/duerme.jpeg"
+    description:
+      "Escribe lo que sientes, haz pausas de redes y date permiso de descansar.",
+    imgUrl: "../img/escribeloquesiento.jpeg",
   },
   {
     name: "Mantente saludable",
     description: "Duerme lo suficiente, hidrátate y mueve tu cuerpo día a día.",
-    imgUrl: "../img/duerme.jpeg"
+    imgUrl: "../img/duerme.jpeg",
   },
   {
     name: "Expresa tu creatividad",
-    description: "Dibuja, pinta o cocina algo nuevo sin buscar que sea perfecto.",
-    imgUrl: "../img/pintar.jpeg"
+    description:
+      "Dibuja, pinta o cocina algo nuevo sin buscar que sea perfecto.",
+    imgUrl: "../img/pintar.jpeg",
   },
   {
     name: "Engríete",
-    description: "Toma una ducha relajante, respira profundo y disfruta pequeños placeres.",
-    imgUrl: "../img/tomaunaducha.jpeg"
+    description:
+      "Toma una ducha relajante, respira profundo y disfruta pequeños placeres.",
+    imgUrl: "../img/tomaunaducha.jpeg",
   },
   {
     name: "Desconéctate un rato",
     description: "Apaga el celular una hora y disfruta el momento presente.",
-    imgUrl: "../img/apagaelcel.jpeg"
+    imgUrl: "../img/apagaelcel.jpeg",
   },
   {
     name: "Rutina de amor propio",
-    description: "Cuida tu piel, ponte tu perfume favorito o arréglate solo para ti.",
-    imgUrl: "../img/rutina.jpeg"
+    description:
+      "Cuida tu piel, ponte tu perfume favorito o arréglate solo para ti.",
+    imgUrl: "../img/rutina.jpeg",
   },
   {
     name: "Lee algo que te inspire",
     description: "Desde un libro hasta frases bonitas en Pinterest.",
-    imgUrl: "../img/leealgoqueteinspire.jpeg"
+    imgUrl: "../img/leealgoqueteinspire.jpeg",
   },
   {
     name: "Conecta con la naturaleza",
     description: "Pasea, siéntate al sol o acaricia a tu mascota.",
-    imgUrl: "../img/naturaleza.jpeg"
+    imgUrl: "../img/naturaleza.jpeg",
   },
-
 ];
 
 let contenedor = document.getElementById("tarjetas-container");
