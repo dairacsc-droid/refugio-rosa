1f2f7013a4f00651a9d412b7d7f31bf5d0d1802d
maysadalila <maydepazcardenas@crackthecode.la>
2025-11-20 10:58:22 -0500

    wa

--- mi-pagina-react/src/components/Footer.jsx
+++ mi-pagina-react/src/components/Footer.jsx
@@ -21,7 +21,7 @@ function Footer() {
           </h3>
           <ul className="space-y-2 text-rose-600">
             <li>
-              <a href="/" className="hover:text-rose-500 transition">
+              <a href="#" className="hover:text-rose-500 transition">
                 Inicio
               </a>
             </li>
